7cf54590eeee6111c69234213022420dd6d3064d
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-09-11 11:56:09 +0530

    dynamic user-agent implemented

--- constants.ts
+++ constants.ts
@@ -102,8 +102,6 @@ export const CLOUD_NAME = "cloudName";
 export const ORG_ID = "organisationId";
 export const UTM_DETAILS =
 	"utm_source=figma&utm_medium=plugin&utm_campaign=watermarkremoverio";
-export const INTEGRATION_PLATFORM =
-	"PixelbinWatermarkRemover/7 (Figma/116.1.3)";
 
 export const createSignedURlDetails = {
 	path: "__figma/ebg",
--- ui/App.tsx
+++ ui/App.tsx
@@ -7,7 +7,6 @@ import {
 	EVENTS,
 	createSignedURlDetails,
 	uploadOptions,
-	INTEGRATION_PLATFORM,
 } from "./../constants";
 import { Util } from "./../util.ts";
 import "./styles/style.scss";
@@ -36,7 +35,7 @@ function App() {
 		new PixelbinConfig({
 			domain: `${API_PIXELBIN_IO}`,
 			apiSecret: `${tokenValue}`,
-			integrationPlatform: INTEGRATION_PLATFORM,
+			integrationPlatform: Util.generateUserAgent(),
 		})
 	);
 
